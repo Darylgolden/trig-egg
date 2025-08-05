46e4c2586ff2ee41696cf05e1e04dd2aedb40be9
Daryl <darylgolden@gmail.com>
2025-08-05 15:58:45 +0800

    attempt to run

--- TrigEgg/Basic.lean
+++ TrigEgg/Basic.lean
@@ -1,1 +1,13 @@
-def hello := "world"
+import Egg
+
+example : 0 = 0 := by
+  egg
+
+example (a b c : Nat) (h₁ : a = b) (h₂ : b = c) : a = c := by
+  egg [h₁, h₂]
+
+open List in
+example (as bs : List α) : reverse (as ++ bs) = (reverse bs) ++ (reverse as) := by
+  induction as generalizing bs with
+  | nil  => egg [reverse_nil, append_nil, List.append]
+  | cons => egg [*, append_assoc, reverse_cons, List.append]
